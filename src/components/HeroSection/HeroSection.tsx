d89355c1abd61aee8e9d178102f32fc71ec69a65
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-09-29 01:44:43 -0300

    style: install and apply sass & style hero section

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,11 +1,16 @@
+import './HeroSection.scss'
+
 function HeroSection() {
   return (
-    <section>
-      <h1>Front-End Developer</h1>
-      <p>
-        Elevating Frontend Development:
-        with Originality and Distinctive Design
-      </p>
+    <section className="hero">
+      <p>Front-End Developer / Full-stack student</p>
+      <div>
+         <h1>
+          Beyond the Code:
+         </h1>
+        <p><span className='purple-text'>Original</span> and <span className='purple-text'>Modern</span></p>
+        <p>Creative Solutions</p>
+      </div>
       <p>Gabriel Moises Alves</p>
     </section>
   )
