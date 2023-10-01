76be487743727af11d6e9d2e179c74fe4b4373cc
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-01 20:33:03 -0300

    feat(reveal-animation): install & apply hero section elements reveal animations

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,19 +1,27 @@
-import './HeroSection.scss'
+import "./HeroSection.scss";
+import { Fade } from "react-awesome-reveal";
 
 function HeroSection() {
   return (
     <section className="hero">
-      <p>Front-End Developer / Full-stack student</p>
-      <div>
-         <h1>
-          Beyond the Code:
-         </h1>
-        <p><span className='purple-text'>Original</span> and <span className='purple-text'>Modern</span></p>
-        <p>Creative Solutions</p>
-      </div>
-      <p>Gabriel Moises Alves</p>
+      <Fade direction="up" delay={500}>
+        <p>Front-End Developer / Full-stack student</p>
+      </Fade>
+      <Fade direction="up" delay={300}>
+        <div className="intro-phrase-container">
+          <h1>Beyond the Code:</h1>
+          <p>
+            <span className="purple-text">Original</span> and{" "}
+            <span className="purple-text">Modern</span>
+          </p>
+          <p>Creative Solutions</p>
+        </div>
+      </Fade>
+      <Fade direction="up" delay={500}>
+        <p>Gabriel Moises Alves</p>
+      </Fade>
     </section>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
