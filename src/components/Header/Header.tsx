36d1697b9ad9825608c8e0707e910b0c6a11b935
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-07 21:28:05 -0300

    refactor: remove burguer menu elements from header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,9 @@
-import { push as Menu } from "react-burger-menu";
 import moonIcon from "../../assets/icons/moon-svgrepo-com.svg";
 import "./Header.scss";
 
 function Header() {
   return (
     <header>
-      <Menu burgerBarClassName="fade-in">
-        <a className="menu-item" href="/">
-          Home
-        </a>
-        <a className="menu-item" href="/about">
-          About
-        </a>
-        <a className="menu-item" href="/contact">
-          Contact
-        </a>
-      </Menu>
       <img src={moonIcon} alt="Moon Icon" className="moon-icon fade-in" />
     </header>
   );
