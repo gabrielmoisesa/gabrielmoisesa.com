7fd8dfa64bd3f842af00cc8786ed96b007d45055
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-07 21:50:54 -0300

    feat: create burguer component & burguer button element and styles

---
+++ src/components/Burguer/Burguer.tsx
@@ -0,0 +1,15 @@
+import './Burguer.scss'
+
+function Burguer() {
+  return (
+    <div>
+      <button className='burguer-button'>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
+  )
+}
+
+export default Burguer
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import moonIcon from "../../assets/icons/moon-svgrepo-com.svg";
+import Burguer from "../Burguer/Burguer";
 import "./Header.scss";
 
 function Header() {
   return (
     <header>
+      <Burguer />
       <img src={moonIcon} alt="Moon Icon" className="moon-icon fade-in" />
     </header>
   );
