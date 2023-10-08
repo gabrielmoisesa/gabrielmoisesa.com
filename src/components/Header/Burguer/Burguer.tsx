c442576e188208b17f656565aaf968f0abaf0c89
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-07 23:56:59 -0300

    feat: add cross animation to open burguer

--- src/components/Header/Burguer/Burguer.tsx
+++ src/components/Header/Burguer/Burguer.tsx
@@ -3,10 +3,11 @@ import './Burguer.scss'
 import MenuContext from '../../../context/MenuContext'
 
 function Burguer() {
-  const toggleMenu = useContext(MenuContext).toggleMenu
+  const { open, toggleMenu } = useContext(MenuContext)
+  const isOpen = open && 'cross-burguer'
 
   return (
-      <button onClick={toggleMenu} className='burguer-button fade-in'>
+      <button onClick={toggleMenu} className={`burguer-button fade-in ${isOpen}`}>
         <span></span>
         <span></span>
         <span></span>
