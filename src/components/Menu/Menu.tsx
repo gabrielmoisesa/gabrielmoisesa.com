c7f647534c0bf96d7b89070272dbf59a42063576
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-07 22:33:00 -0300

    feat: create menu component & initial content

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,19 @@
+import './Menu.scss'
+
+function Menu() {
+  return (
+    <div className='menu-container'>
+      <a href="/">
+        About me
+      </a>
+      <a href="/">
+        Projects
+      </a>
+      <a href="/">
+        Contact
+      </a>
+    </div>
+  );
+}
+
+export default Menu;
