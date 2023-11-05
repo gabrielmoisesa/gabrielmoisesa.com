9fda47743544771dbf2f9d67a73c55ad74dbb4b8
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-05 16:46:12 -0300

    chore: reinstall and reconfigure eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.scss";
-import MenuProvider from "./context/MenuProvider.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.scss';
+import MenuProvider from './context/MenuProvider.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MenuProvider>
       <App />
     </MenuProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
